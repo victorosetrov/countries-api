1d439b9235856071063604fe6635c7664dbbafb7
Viktor <victorosetrov@github.com>
2021-03-29 17:38:52 +0100

    revised error with preview.

--- script.js
+++ script.js
@@ -5,23 +5,21 @@ const countriesContainer = document.querySelector(".countries");
 
 const renderCountry = function (data, className = "") {
   const html = `
-        <article class="country">
-        <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-      </article>
-    `;
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
   countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.getElementsByClassName.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 // const getCountryDataAndNeighbour = function (country) {
@@ -66,4 +64,4 @@ const getCountryData = function (country) {
     .then((response) => response.json())
     .then((data) => renderCountry(data[0]));
 };
-getCountryData("portugal");
+getCountryData("ireland");
